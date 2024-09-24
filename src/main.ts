5b1ff050fe611ab306b72dd3301a784bf1abc43d
ali <alimanssouri221@gmail.com>
2024-09-24 22:08:22 +0330

    refactor: Update dependencies and add Swagger documentation
    Update package-lock.json and package.json to include new dependencies for Swagger documentation. Also, modify the main.ts file to configure Swagger documentation using the @nestjs/swagger module.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .addTag('users')
+    .addTag('payments')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
